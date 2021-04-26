07c9733d488c1b371d498e06abd9e53d86ee0125
Soham <soham.chattopadhyay@cognizant.com>
2021-04-26 17:58:10 +0530

    datepicker wip

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'EStocksManagerWeb';
+  selectedDate = '';
+
+  onDateEntry(event: any) {
+    console.log(event);
+  }
 }
