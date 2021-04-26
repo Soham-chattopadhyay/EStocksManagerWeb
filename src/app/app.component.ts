a6895307cfc5186894ced335bb552a515c4b1733
Soham <soham.chattopadhyay@cognizant.com>
2021-04-26 22:03:14 +0530

    Added section to show stock info table

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'EStocksManagerWeb';
+  
+  //Properties
   selectedFromDate = '';
   selectedToDate = '';
+  stockInfoHeaders = ['Stock Price','Date','TIME'];
+  stockInfoRows = [
+    {
+      "companyCode": "APL",
+      "stockID": "AAPL",
+      "stockPrice": 126128,
+      "priceAsOfDate": "04-23-2021",
+      "priceAsOfTime": "16:51:33"
+    },
+    {
+      "companyCode": "APL",
+      "stockID": "AAPL",
+      "stockPrice": 126129,
+      "priceAsOfDate": "04-23-2021",
+      "priceAsOfTime": "16:51:39"
+    },
+    {
+      "companyCode": "APL",
+      "stockID": "AAPL",
+      "stockPrice": 126116,
+      "priceAsOfDate": "04-23-2021",
+      "priceAsOfTime": "16:51:47"
+    },
+    {
+      "companyCode": "APL",
+      "stockID": "AAPL",
+      "stockPrice": 134251,
+      "priceAsOfDate": "04-23-2021",
+      "priceAsOfTime": "16:55:50"
+    },
+    {
+      "companyCode": "APL",
+      "stockID": "AAPL",
+      "stockPrice": 14526,
+      "priceAsOfDate": "04-25-2021",
+      "priceAsOfTime": "08:16:08"
+    },
+    {
+      "companyCode": "APL",
+      "stockID": "AAPL",
+      "stockPrice": 14579,
+      "priceAsOfDate": "04-25-2021",
+      "priceAsOfTime": "08:16:19"
+    }
+  ];
+
+  //#region  Get Date Range
 
   FromDateEntry(event: any) {
     console.log(event.target.valueAsDate);
@@ -22,4 +70,7 @@ export class AppComponent {
     this.selectedToDate = formatDate(event.target.valueAsDate, 'MM-dd-yyyy', 'en-US');
     console.log('selectedToDate', this.selectedToDate);
   }
+
+  //#endregion
+
 }
