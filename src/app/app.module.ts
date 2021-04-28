2ee95d9d34e79b6f5fe549505a509591eb6b8ed3
Soham <soham.chattopadhyay@cognizant.com>
2021-04-28 22:58:52 +0530

    stock-details component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http'
 import { stockmanagementapiservice } from './Services/stockmanagementapi.services';
+import { StockDetailsComponent } from './stock-details/stock-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StockDetailsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/stock-details/stock-details.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { CompanyStocks } from '../Models/CompanyStocks';
+
+@Component({
+  selector: 'app-stock-details',
+  templateUrl: './stock-details.component.html',
+  styleUrls: ['./stock-details.component.css']
+})
+export class StockDetailsComponent implements OnInit {
+  @Input() stocksInfoHidden = true;
+  @Input() stockInfo = new CompanyStocks();
+
+  stockInfoHeaders = ['Stock Price','Date','TIME'];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
