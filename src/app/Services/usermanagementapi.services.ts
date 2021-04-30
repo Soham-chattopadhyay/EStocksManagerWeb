b37cdd66ad176e648a4721c0a1792774909c4d7c
Soham <soham.chattopadhyay@cognizant.com>
2021-04-30 14:50:17 +0530

    login and sign-up page

--- src/app/Services/usermanagementapi.services.ts
+++ src/app/Services/usermanagementapi.services.ts
@@ -12,14 +12,14 @@ export class usermanagementapiservice
         return this.httpClient.post('http://localhost:53870/api/v1.0/market/user/add', user, {responseType: 'text' as 'json'});
     }
 
-    // getCompanyInfo(comnapnyCode: string): Observable<any> {
-    //     return this.httpClient.get('http://localhost:53870/api/v1.0/market/company/info/' + comnapnyCode );
-    // }
+    getUserIDAvailability(userID: string): Observable<any> {
+        return this.httpClient.get('http://localhost:53870/api/v1.0/market/user/verifyUserID/' 
+        + userID, {responseType: 'text' as 'json'});
+    }
 
-    // getStockyInfo(comnapnyCode: string, startDate: string, toDate: string): Observable<any> {
-    //     return this.httpClient.get('http://localhost:53870/api/v1.0/market/stock/get/' 
-    //     + comnapnyCode
-    //     + '/' + startDate
-    //     + '/' + toDate );
-    // }
+    getUserInfo(userID: string, passowrd: string): Observable<any> {
+        return this.httpClient.get('http://localhost:53870/api/v1.0/market/user/get/' 
+        + userID
+        + '/' + passowrd, {responseType: 'text' as 'json'});
+    }
 }
--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -1,7 +1,17 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
-import { UserProfile } from '../Models/UserProfile';
-import { usermanagementapiservice } from '../Services/usermanagementapi.services';
+import {
+  Component,
+  OnInit,
+  ViewChild
+} from '@angular/core';
+import {
+  Router
+} from '@angular/router';
+import {
+  UserProfile
+} from '../Models/UserProfile';
+import {
+  usermanagementapiservice
+} from '../Services/usermanagementapi.services';
 
 @Component({
   selector: 'app-login-page',
@@ -10,57 +20,131 @@ import { usermanagementapiservice } from '../Services/usermanagementapi.services
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor(private _usermanagementapiservice: usermanagementapiservice, private router: Router ){};
+  constructor(private _usermanagementapiservice: usermanagementapiservice, private router: Router) {};
+  @ViewChild('signuserID') inputUserID: any;
+  @ViewChild('signPassword') inputPassword: any;
+  @ViewChild('reguserName') regUserName: any;
+  @ViewChild('reguserID') regUserID: any;
+  @ViewChild('regPassword') regPassword: any;
 
   //Properties
   userID: string;
   userName: string;
   password: string;
+  showSignInPage = true;
+  notVerified = true;
+  hideRegError = true;
 
-  ngOnInit(): void {
-  }
-  
+  ngOnInit(): void {}
+
   setUserProfile(event: any, field: string) {
-    if( field == 'ID')
-    {
+    if (field == 'ID') {
       this.userID = event.target.value;
-    }
-    else if(field == 'NAME')
-    {
+    } else if (field == 'NAME') {
       this.userName = event.target.value;
-    }
-    else
-    {
+    } else {
       this.password = event.target.value;
     }
   }
-  
-  registerNewUser() {
-  //Test request data
-  var userProfile = new UserProfile();
-  userProfile.userID = this.userID;
-  userProfile.userName = this.userName;
-  userProfile.password = this.password;
 
-  console.log('userProfile', userProfile);
-  this._usermanagementapiservice.registerUser(userProfile)
-    .subscribe
-    (
-      data=>
-      {
-        this.userID = data;
-        console.log('UserID', data);
-        if(data)
-        {
-          this.navigateToCompanyDetails();
+  showRegistrationForm() {
+    this.showSignInPage = false;
+    this.inputUserID.nativeElement.value = '';
+    this.regUserName.nativeElement.value = '';
+    this.inputPassword.nativeElement.value = '';
+    this.regUserID.nativeElement.value = '';
+    this.regPassword.nativeElement.value = '';
+  }
+
+  registerNewUser() {
+
+    var userProfile = new UserProfile();
+    userProfile.userID = this.userID;
+    userProfile.userName = this.userName;
+    userProfile.password = this.password;
+
+    console.log('userProfile', userProfile);
+    this._usermanagementapiservice.registerUser(userProfile)
+      .subscribe(
+        data => {
+          this.userID = data;
+          console.log('UserID', data);
+          if (data) {
+            this.showSignInPage = true;
+          } else {
+            this.inputUserID.nativeElement.value = '';
+            this.regUserName.nativeElement.value = '';
+            this.inputPassword.nativeElement.value = '';
+            this.regUserID.nativeElement.value = '';
+            this.regPassword.nativeElement.value = '';
+          }
         }
-      }
-    );
+      );
+  }
+
+  validateLogin() {
+    if (this.userID && this.password) {
+      this._usermanagementapiservice.getUserInfo(this.userID, this.password)
+        .subscribe(
+          data => {
+            this.userID = data;
+            console.log('UserID', data);
+            if (data) {
+              console.log('User Validated');
+              this.navigateToCompanyDetails();
+            } else {
+              console.log('Invalid user');
+              this.inputUserID.nativeElement.value = '';
+              this.regUserName.nativeElement.value = '';
+              this.inputPassword.nativeElement.value = '';
+              this.regUserID.nativeElement.value = '';
+              this.regPassword.nativeElement.value = '';
+              this.notVerified = false;
+            }
+          },
+          err => {
+            console.log('HTTP Error', err);
+            this.notVerified = false;
+          }
+        );
+    } else {
+      alert('Please enter UserID and Passowrd!')
+    }
 
   }
 
-  navigateToCompanyDetails () {
-    this.router.navigate( ['company-detail'] );
+  findUser() {
+    if (this.userID) {
+      if (this.userID.length > 3) {
+        this._usermanagementapiservice.getUserIDAvailability(this.userID)
+          .subscribe(
+            data => {
+              this.userID = data;
+              console.log('UserID', data);
+              if (data) {
+                console.log('User Found');
+                this.inputUserID.nativeElement.value = '';
+                this.regUserName.nativeElement.value = '';
+                this.inputPassword.nativeElement.value = '';
+                this.regUserID.nativeElement.value = '';
+                this.regPassword.nativeElement.value = '';
+                this.hideRegError = false;
+              }
+            },
+            err => {
+              console.log('HTTP Error', err);
+            }
+          );
+      } else {
+        alert('User ID must be greater than 3 characters!');
+      }
+    } else {
+      alert('Please enter UserID!')
+    }
+  }
+
+  navigateToCompanyDetails() {
+    this.router.navigate(['company-detail']);
   }
 
 }
