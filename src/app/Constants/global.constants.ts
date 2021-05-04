7f056d798466245849879c37c9bd09bd2e6ba229
Soham <soham.chattopadhyay@cognizant.com>
2021-05-04 23:43:57 +0530

    modified constants

--- src/app/Constants/global.constants.ts
+++ src/app/Constants/global.constants.ts
@@ -1,3 +1,4 @@
+//Gateway API
 export const appConstants = {
     API_BASE_URI: 'https://ocelot-gate-api.azurewebsites.net/',
     API_COMPANY_BASE_ROUTE: 'api/market/company/',
@@ -20,3 +21,27 @@ export const appConstants = {
     VERIFY_USER_ID: 'verifyUserID/',
     DELETE_USER: 'delete/'
 }
+
+//Direct API
+// export const appConstants = {
+//     API_BASE_URI: 'https://estockmanagerapi.azurewebsites.net/',
+//     API_COMPANY_BASE_ROUTE: 'api/v1.0/market/company/',
+//     API_STOCK_BASE_ROUTE: 'api/v1.0/market/stock/',
+//     API_USER_BASE_ROUTE: 'api/v1.0/market/user/',
+
+//     //Company API endpoints
+//     REGISTER_COMPANY: 'register',
+//     COMPANY_INFO: 'info/',
+//     ALL_COMPANY_INFO: 'getall',
+//     DELETE_COMPANY: 'delete/',
+
+//     //Stock API endpoints
+//     ADD_STOCK: 'add/',
+//     GET_STOCK_INFO: 'get/',
+
+//     //User API endpoints
+//     REGISTER_USER: 'add',
+//     GET_USER: 'get/',
+//     VERIFY_USER_ID: 'verifyUserID/',
+//     DELETE_USER: 'delete/'
+// }
